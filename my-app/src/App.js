fab367758fbc1090b7f747ce69610ab50fcfa86d
Smit <ptlsmit.b@gmail.com>
2022-07-15 09:16:41 -0700

    final commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import data from "./data";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
   const [people, setPeople] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(2);
 
+
+useEffect(() => {
+  let lastIndex = people.length-1
+  if(index < 0){
+    setIndex(lastIndex)
+  }
+  if(index > lastIndex){
+    setIndex(0)
+  }
+}, [index,people]);
+
+useEffect(() => {
+  let slider = setInterval (() =>{
+    setIndex(index + 1)
+  },3000)
+  return () => clearInterval(slider)
+}, [index]);
   return (
     <section className="section">
       <div className="title">
@@ -37,10 +54,10 @@ const App = () => {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft/>
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight/>
         </button>
       </div>
